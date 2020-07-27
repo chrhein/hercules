f80369cd931f58dee8c8991b5180a0ea7dc20119
Christian Hein <scheiehein@gmail.com>
2020-07-27 15:14:58 +0200

    Hercules Cool Colors

--- colors.js
+++ colors.js
@@ -1,3 +1,7 @@
+/*
+
+# Color Palette 1
+
 module.exports = {
   background: '#FFFFFF',
   backgroundDark: '#f0e6f6',
@@ -11,4 +15,27 @@ module.exports = {
   secondary: '#ff4081',
   secondaryLight: '#ff79b0',
   secondaryDark: '#c60055',
+};
+
+*/
+
+/*
+
+# Color Palette 2
+
+*/
+
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#e7eaef',
+
+  text: '#080b11',
+
+  primary: '#162229',
+  primaryLight: '#737a7e',
+  primaryDark: '#0d1418',
+
+  secondary: '#05828e',
+  secondaryLight: '#69b4bb',
+  secondaryDark: '#034e55',
 };
