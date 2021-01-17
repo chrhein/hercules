abac71686027117d3deb78f6e57f1dacd3057e4a
Christian Hein <scheiehein@gmail.com>
2021-01-17 16:41:20 +0100

    Experience and Education looks good now, missing Volunteer Experiences

---
+++ src/data/resume-pages/Education.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const Education = () => (
+  <div className="row education">
+    <div className="three columns header-col">
+      <h1>
+        <span>Education</span>
+      </h1>
+    </div>
+
+    <div className="nine columns main-col">
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>Master&apos;s Degree in Informatics specializing in Visualization</h3>
+          <p className="info">
+            University of Bergen
+            <span className="bullet">&bull;</span>
+            <em className="date">August 2020 - Present</em>
+          </p>
+
+          <p>
+            A Master&apos;s Degree that results in great knowledge and advanced skills in visualization, computer graphics, and interactive techniques, as well as good skills in related programming.
+          </p>
+        </div>
+      </div>
+
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>Bachelor&apos;s Degree in Computer Security</h3>
+          <p className="info">
+            University of Bergen
+            <span className="bullet">&bull;</span>
+            <em className="date">August 2017 - June 2020</em>
+          </p>
+
+          <p>
+            The Bachelor&apos;s Degree included both the general Computer Science subjects offered by the University of Bergen, as well as a specialization in Computer Security.
+          </p>
+        </div>
+      </div>
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>Initial Compulsory Military Service</h3>
+          <p className="info">
+            Royal Norwegian Air Force
+            <span className="bullet">&bull;</span>
+            <em className="date">July 2016 - July 2017</em>
+          </p>
+
+          <p>
+            Worked as an operator in an alarm central at Ørland Air Station, in shifts of 12 hours a day, 14 days in a row.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Education;
